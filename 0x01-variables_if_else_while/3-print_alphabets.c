2e93a176eee6cd24e9c7cf9975b65eb7f134670d
techshak <jmweltokg@gmail.com>
2022-12-12 14:22:20 -0800

    Mandatory Half

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * prints the alphabet in lowercase, followed by a new line
+ * Return 0
+ */
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main:Entry Point
+ * Description: Prints the alphabet in lowercase follwed
+ * by a new line, except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
